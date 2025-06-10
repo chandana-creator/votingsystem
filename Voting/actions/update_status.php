c8649004963b994e1f20db8f25dc9c98ec66b811
chandana-creator <chandanabsomashekara@gmail.com>
2025-06-10 19:01:47 +0530

    Delete Voting directory

--- Voting/actions/update_status.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('../actions/connect.php');
-
-// Update election status where end_date has passed
-$query = "UPDATE election SET status = 'closed' WHERE end_date < CURDATE() AND status != 'closed'";
-mysqli_query($con, $query);
-?>
